a56d8b90d57bed5dfb44e70478987c60db21dd86
prakash-khadka <pkhadka2000ak@yahoo.com>
2022-12-14 13:53:06 +0545

    short description in admin pannel

--- rental/inventory/admin.py
+++ rental/inventory/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 from rental.inventory.models import Product
+from django.template.defaultfilters import truncatechars
+
+
+def short_description(self):
+    return truncatechars(self.description, 35)
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'web_id', 'description',
+    list_display = ['id', 'name', 'web_id', 'short_description',
                     'slug', 'is_active']
     search_fields = ['name', 'description', 'web_id', ]
-    list_per_page = 20
+    list_per_page = 10
     list_filter = ['name', 'created_at']
 
+    def short_description(self, obj):
+        return obj.description[:50]
