49a940aa039086cc9ff634db3a1e48b9a922c544
prakash-khadka <pkhadka2000ak@yahoo.com>
2022-12-16 19:23:17 +0545

    home and cat pages

--- rental/urls.py
+++ rental/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rental.demo import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
     path('demo/', include("rental.demo.urls", namespace="demo")),
 ]
